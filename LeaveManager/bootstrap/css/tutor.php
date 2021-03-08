af2e9098b183fbc8248ce9d38180d4558f634467
gazola04 <76808246+gazola04@users.noreply.github.com>
2021-03-07 20:51:02 -0500

    Initial commit

---
+++ LeaveManager/bootstrap/css/tutor.php
@@ -0,0 +1,28 @@
+<?php
+include_once("header.php");
+?>
+<div class="container my-5">
+    <div class="card">
+        <h1 class="text-center">Why PHP and MySQL</h1>
+        <p>The programming languages used in the implementation of this project are PHP (Hypertext Preprocessor) and MYSQL programming languages. PHP is a general purpose server side scripting language originally designed for web development to produce dynamic web pages. It has also evolved to include a command line interface capability and can be used in stand-alone graphical applications.
+	The following features make PHP a preferred implementation language for this project:
+i.	PHP has its root in C and C++. PHP syntax is most similar to C and C++ language syntax, so programmers find it easy to learn and manipulate.
+ii.	PHP can run on both UNIX and windows. Hence it is compatible across various operating systems.
+iii.	PHP has powerful output buffering that further increases over the output flow.  PHP internally rearranges the buffer so that the header comes before the content.
+</p>
+        <p>MYSQL is a relational database management system written in C and C++, that runs as a server providing multi user access to a number of databases. MYSQL is used basically to create a relational database structure on a server in order to store data or automate procedures. The following features make MYSQL also a preferred implementation language in this research:
+
+i.	MYSQL is written in C and C++ and tested with a broad range of different compilers. It also functions on different platforms.
+ii.	It uses multi-layered server design with independent modules.
+iii.	It is designed to be fully multi-threaded using kernel threads to easily use multiple CPUs if they are available.
+: </p>
+        <ul>
+            <li><i class="fa fa-envelope-open-o"></i> Email: php.org</li>
+            <li><i class="fa fa-phone"></i> Phone: 09000000000</li>
+        </ul>
+       
+    </div>
+</div>
+<?php
+include_once("footer.php");
+?>
